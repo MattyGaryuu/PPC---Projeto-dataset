8b8d1a41378d14ade7c4bf0c092d426b173e22a2
Matty <Matty@DESKTOP-M0K4I8C>
2023-11-04 21:43:28 -0300

    luz piscou medo da luz cair e queimar pc

--- Project1/openmp.cpp
+++ Project1/openmp.cpp
@@ -5,44 +5,53 @@
 #include <vector>
 #include<chrono>
 
-using namespace std;
+std::fstream dataset;
+std::vector<std::vector<std::string>> m_dataset; //matriz que ira armazenar o dataset
+int Pointer = 1; //ponteiro para orientar onde o dataset parou de ler, pular primeira linha do dataset que armazena os nomes das colunas.
+std::string Line; //string responsavel por armazenar a linha que atualmente esta sendo lida
 
+
+void CreateDictArchive() { //função para a criação dos arquivos de dicionario
+    #pragma omp parallel //Criar os Dicionarios baseado nas colunas
+        {
+            //nesse caso a diferenca usando open mp foi qnula, dado que leitura e escrita de dados geralmente nao faz muita diferenca ser paralelo
+            if (dataset.is_open()) {
+                if (std::getline(dataset, Line)) {
+                    std::cout << "A primeira linha do arquivo é: " << Line << std::endl;
+                    int num_column = 0; //descobrir a quantidade de coluna
+                    std::istringstream StringLine(Line);
+                    std::string data; //armazenar os valores separados por virguma
+                    while (std::getline(StringLine, data, ',')) { //pegar o valor antes das/entre as virgula e armazenar em data
+                        num_column++; // Conta as colunas
+                        std::fstream dict("dicts/" + data + ".csv", std::ios::in | std::ios::out | std::ios::app);
+                        dict << "codigo,descrição" << '\n';
+                        dict.close();
+                    }
+                    std::cout << "Numero de Colunas: " << num_column << std::endl;
+                }
+                else {
+                    std::cerr << "O arquivo está vazio." << std::endl;
+                }
+    
+                dataset.close();
+            }
+            else {
+                std::cerr << "Não foi possível abrir o arquivo." << std::endl;
+            }
+        }
+}
+
+void ReadCsv() {
+    std::string line;
+
+}
 
 int main() {
     auto start_time = std::chrono::high_resolution_clock::now();
     //std::ifstream arquivo("sample.csv"); //teste
-    std::ifstream arquivo("testeMaior.csv"); //arquivo
+    dataset.open("testeMaior.csv", std::ios::in | std::ios::out | std::ios::app); //arquivo
 
-#pragma omp parallel //Criar os Dicionarios baseado nas colunas
-    {
-        //nesse caso a diferenca usando open mp foi quase nula, foi tentado fazer um pushback no while e depois um parallel for, mas o tempo de execucao piorou
-        if (arquivo.is_open()) {
-            std::string Line; //string responsavel por armazenar a linha
-            if (std::getline(arquivo, Line)) {
-                std::cout << "A primeira linha do arquivo é: " << Line << std::endl;
-                int num_column = 0; //descobrir a quantidade de coluna
-                std::istringstream StringLine(Line);
-                std::string data; //armazenar os valores separados por virguma
-                while (std::getline(StringLine, data, ',')) { //pegar o valor antes das/entre as virgula e armazenar em data
-                    num_column++; // Conta as colunas
-                    std::fstream dict("dicts/" + data + ".csv", std::ios::in | std::ios::out | std::ios::app);
-                    dict << "codigo,descrição" << '\n';
-                    if (arquivo.is_open()) {
-                        arquivo.close();
-                    }
-                }
-                std::cout << "Numero de Colunas: " << num_column << std::endl;
-            }
-            else {
-                std::cerr << "O arquivo está vazio." << std::endl;
-            }
-
-            arquivo.close();
-        }
-        else {
-            std::cerr << "Não foi possível abrir o arquivo." << std::endl;
-        }
-    }
+    CreateDictArchive();
 
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
