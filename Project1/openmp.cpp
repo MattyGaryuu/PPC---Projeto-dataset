ee8920c2beb3321d5990784cb2150f601ecfb3f2
Matheus Henrique Nobre de Freitas <matheus.freitas@unisantos.br>
2023-11-06 16:23:18 -0300

    comentando código

--- Project1/openmp.cpp
+++ Project1/openmp.cpp
@@ -24,17 +24,12 @@ void CreateDictArchive() { //função para a criação dos arquivos de dicionario
             //nesse caso a diferenca usando open mp foi qnula, dado que leitura e escrita de dados geralmente nao faz muita diferenca ser paralelo
             if (dataset.is_open()) {
                 if (std::getline(dataset, Line)) {
-                    std::cout << "A primeira linha do arquivo é: " << Line << std::endl;
+                    //std::cout << "A primeira linha do arquivo é: " << Line << std::endl;
                     std::istringstream StringLine(Line);
                     std::string data; //armazenar os valores separados por virguma
                     while (std::getline(StringLine, data, ',')) { //pegar o valor antes das/entre as virgula e armazenar em data
-                        //dict.open("dicts/" + data + ".csv");
-                        //dict.imbue(std::locale(""));
                         dictindex.push_back("dicts/" + data + ".csv");
-                        //dict << "codigo,descrição" << '\n';
-                        //dict.close();
                     }
-                    std::cout << "Numero de Colunas: " << dictindex.size() << std::endl;
                 }
                 else {
                     std::cerr << "O arquivo está vazio." << std::endl;
@@ -80,34 +75,39 @@ void ReadCsv() {
 }
 
 bool Verify(int value, std::string VD) {
-    //#pragma omp parallel for
+    //procura na propria matriz temporaria se o valor aparece
     for (int i = 0; i < m_a.size(); i++) {
         if (m_a[i] == VD) {
-            return false;
+            return false; //está na lista
         }
     }
     return true; // nao esta na lista
 }
 
 void Editdict() {
-    for (int i = 0; i < dictindex.size(); i++) {
-        Daux = 0;
+    //Afim de poder aplicar uma paralelização no código, o grupo decidiu por fazer uma função que usa-se de um vetor temporario para o armazenamento de dados de cada coluna, verificando
+    // se ele não aparece no arquivo, se por acaso não aparecer um novo valor sera armazenado no vetor m_a. Ao final da função será armazenado num arquivo .csv todos os ids.
+    //
+    //O Grupo tentou também fazer uma função acessando o arquivo ao ines de um vetor auxiliar mas, como já dito, era inviavel dado que leitura e escrita geralmente é feito de maneira
+    //sequencial e essa leitura e escrita afetariam muito o desempenho do programa.
+
+    for (int i = 0; i < dictindex.size(); i++) { //para cada coluna
         m_a.clear();
-        #pragma omp parallel for
-        for (int j = 0; j < m_dataset.size(); j++) {
+        //Após 10 testes com o dataset de 1000 usando um i3 2100 foi percebido que a maneira mais otimizada seria as threads trabalharem a mesma coluna do que mexer simultaneamente em
+        // todas (evitando também uma condição de corrida) media trabalhando cada coluna paralelamente:1431 ms. nedia threads trabalhando na mesma coluna = media:1175. collapse piorou a perfomance
+        #pragma omp parallel for  
+        for (int j = 0; j < m_dataset.size(); j++) { //para cada linha do dataset
             
             //[j] [i]
-            //#pragma omp critical
-            if (Verify(i, m_dataset[j][i])) {
-                Daux++;
+            if (Verify(i, m_dataset[j][i])) { //verificar se não aparece
                 m_a.push_back(m_dataset[j][i]);
-                //std::cout << std::to_string(Daux) +" saf " + m_dataset[j][i] << std::endl;
             }
         }
+        //Após analisado todas linhas, armazenar num arquivo
         dict.open(dictindex[i], std::ios::app);
         #pragma omp parallel for
         for(int j = 0; j < m_a.size(); j++){
-            dict << std::to_string(j+1) + "," + m_a[j] + "\n";
+            dict << std::to_string(j+1) + "," + m_a[j] + "\n"; //sera salvo no arquivo de modo que (index da matriz + 1),(Descrição)
         }
         dict.close();
     }
