680a39aad1f90f6e5e691d5d10096dfdbcd60e1c
Matty <Matty@DESKTOP-M0K4I8C>
2023-11-12 00:22:34 -0300

    Provavel codigo final de open mp

--- Project1/openmp.cpp
+++ Project1/openmp.cpp
@@ -8,18 +8,13 @@
 #include <algorithm>
 #include<omp.h>
 
-#define limit 1000000//00//000
-
+#define limit 15000000
 std::fstream dataset;
+std::fstream wdataset;
 std::vector<std::string> dictindex;
-std::vector<std::string> dictorigin;
 std::vector<std::vector<std::string>> m_dataset; //matriz que ira armazenar o dataset
-int Point = 1; //ponteiro para orientar onde o dataset parou de ler, ja pulando primeira etapa
 std::string Line; //string responsavel por armazenar a linha que atualmente esta sendo lida
 std::fstream dict;
-std::vector<std::string> m_a;
-
-int Daux;
 
 std::vector<std::string> blacklist = { "idatracacao", "idcarga", "nacionalidadearmador", "pesocarga_cntr", "pesocargabruta", "qtcarga", "tatracado", "tesperaatracacao", "tesperacainicioop",
 "tesperadesatracacao", "testadia", "toperacao", "ano"}; //os seguintes nomes sao colunas de valores ja numericos, nao precisando de um tratamento de id para tais
@@ -28,13 +23,11 @@ void CreateDictArchive() { //função para a criação dos arquivos de dicionario
             //nesse caso a diferenca usando open mp foi qnula, dado que leitura e escrita de dados geralmente nao faz muita diferenca ser paralelo
             if (dataset.is_open()) {
                 if (std::getline(dataset, Line)) {
-                    //std::cout << "A primeira linha do arquivo é: " << Line << std::endl;
                     std::istringstream StringLine(Line);
                     std::string data; //armazenar os valores separados por virguma
                     while (std::getline(StringLine, data, ',')) { //pegar o valor antes das/entre as virgula e armazenar em data
                         //tratar black list
                         bool aux = true;
-                        //#pragma omp parallel for
                         for (int i = 0; i < blacklist.size(); i++)
                         {
                             if (data == blacklist[i]) {
@@ -51,7 +44,14 @@ void CreateDictArchive() { //função para a criação dos arquivos de dicionario
                         else {
                             dictindex.push_back("b"); //sinalizador de black list, importante colocar no vetor para que a ordem natural dos outros dados nao fujam do controle
                         }
-                        dictorigin.push_back(data);
+                        if (dictindex.size() == 1) {
+                            wdataset << data;
+                        }
+                        else{
+                            wdataset << "," + data;
+                        }
+                        
+
                     }
                 }
                 else {
@@ -63,149 +63,29 @@ void CreateDictArchive() { //função para a criação dos arquivos de dicionario
             }
 }
 
-void ReadCsv() {
-    //dataset.open("testeMaior.csv", std::ios::in | std::ios::out | std::ios::app);
-        int p_aux = Point;
-        for (int i = p_aux; i < (p_aux + limit); i++) {
-            if(std::getline(dataset, Line)){
+void WriteCsv(int sum) {
 
-                //separar linha unica em varios componentes para a matriz
-
-                    std::istringstream StringLine(Line);
-                    std::string aux;
-                    std::vector<std::string> V_aux;
-                    while (std::getline(StringLine, aux, ',')) {
-                        V_aux.push_back(aux);
-                        //std::cout << aux + ",";
-                    }
-                    //std::cout << std::endl;
-                    m_dataset.push_back(V_aux);
-                    Point++;
-            }
-            else {
-                std::cerr << "O arquivo está vazio." << std::endl;
-                //break;
-            }
-        }
-
-}
-
-bool Verify(int value, std::string VD) {
-    //procura na propria matriz temporaria se o valor aparece
-    for (int i = 0; i < m_a.size(); i++) {
-        if (m_a[i] == VD) {
-            return false; //está na lista
-        }
-    }
-    return true; // nao esta na lista
-}
-
-
-
-void Editdict() {
-    //Afim de poder aplicar uma paralelização no código, o grupo decidiu por fazer uma função que usa-se de um vetor temporario para o armazenamento de dados de cada coluna, verificando
-    // se ele não aparece no arquivo, se por acaso não aparecer um novo valor sera armazenado no vetor m_a. Ao final da função será armazenado num arquivo .csv todos os ids.
-    //
-    //O Grupo tentou também fazer uma função acessando o arquivo ao ines de um vetor auxiliar mas, como já dito, era inviavel dado que leitura e escrita geralmente é feito de maneira
-    //sequencial e essa leitura e escrita afetariam muito o desempenho do programa.
-
-//#pragma omp parallel for //erro kkkk
-    for (int i = 0; i < dictindex.size(); i++) { //para cada coluna
-        if(dictindex[i].size() > 1) //se tiver apenas 1 ou 0 = ignorar
-        {
-            m_a.clear();
-            //Após 10 testes com o dataset de 1000 usando um i3 12100f foi percebido que a maneira mais otimizada seria as threads trabalharem a mesma coluna do que mexer simultaneamente em
-            // todas (alem de dar crash no programa ao tentar) em um teste trabalhando so 50000 linhas a diferença foi de -32% de perfomance. Usando threads para escrever aumentava o tempo em 8%.
-            //com threads (media de 5 testes) = 27843 ms
-            //sem threads (media de 5 testes) = 40005 ms
-            // (Black_list e substituicao de id nao implementada na hora para testar ao maximo, com black list nao passava de 5 segundos, por isso feito para testar ao maximo)
-
-#pragma omp parallel for 
-            for (int j = 0; j < m_dataset.size(); j++) { //para cada linha do dataset         
-                //[j] [i]    
-#pragma omp critical
-                {
-                    if (Verify(i, m_dataset[j][i])) { //verificar se não aparece
-                        m_a.push_back(m_dataset[j][i]);
-                    }
-                }
-            }
-            //Após analisado todas linhas, armazenar num arquivo
-            dict.open(dictindex[i], std::ios::out | std::ios::trunc); //excluir conteudo se ja existir
-            for (int j = 0; j < m_a.size(); j++) {
-                dict << std::to_string(j + 1) + "," + m_a[j] + "\n"; //sera salvo no arquivo de modo que (index da matriz + 1),(Descrição)
-            }
-            dict.close();
-        }
-        
-    }
-}
-
-void ReplaceById() {
-    //substituir por colunas
-
-    for (int i = 0; i < dictindex.size(); i++) {
-        if (dictindex[i].size() > 1)
-        {
-            //abrir arquivo de id
-            std::map<std::string, std::string> ids; //mapa para procura de id
-            dict.open(dictindex[i], std::ios::in);
-
-            if (dict.is_open()) {
-                while (std::getline(dict, Line)) {
-                    std::istringstream stringstream(Line);
-                    std::string key, value;
-
-                    if (std::getline(stringstream, value, ',')) {
-                        if (std::getline(stringstream, key, ',')) {
-                            ids[key] = value;
-                        }
-                    }
-                }
-            }
-
-            dict.close();
-
-            #pragma omp parallel for
-            for (int j = 0; j < m_dataset.size(); j++) {
-                auto aux = ids.find(m_dataset[j][i]);
-                if (aux != ids.end()) {
-                    m_dataset[j][i] = aux->second;
-                }
-            }
-        }
-    }
-}
-
-void WriteCsv() {
-    std::fstream wdataset("dataset_00_sem_virg_final.csv", std::ios::app);
-    wdataset << dictorigin[0];
-    for (int i = 1; i < dictorigin.size(); i++) {
-        wdataset << "," + dictorigin[i];
-    }
-    wdataset << '\n';
-    //#pragma omp parallel for
     for (int i = 0; i < m_dataset.size(); i++) {
         Line = m_dataset[i][0];
         for (int j = 1; j < m_dataset[i].size(); j++) {
             Line += "," + m_dataset[i][j];
         }
-        //#pragma omp critical
         wdataset << Line + '\n';
     }
-    wdataset.close();
+    
 }
 
-void teste() {
+void datasetManipulation() {
+#pragma omp parallel for shared(dictindex)
     for (int i = 0; i < dictindex.size(); i++) {
+        if (dictindex[i].size() > 1) {
+            std::map<std::string, int> ids;
+            ids[""] = 0;
 
-        if (dictindex[i].size() > 1)
-        {
-            //abrir arquivo de id
-            std::map<std::string, int> ids; //mapa para procura de id
+            std::ifstream dict(dictindex[i]);
+            std::string Line;
+            std::getline(dict, Line); // Não ler a primeira linha
 
-
-            dict.open(dictindex[i], std::ios::in | std::ios::out | std::ios::app);
             while (std::getline(dict, Line)) {
                 std::istringstream StringLine(Line);
                 std::string value, key;
@@ -216,35 +96,32 @@ void teste() {
             }
             dict.close();
 
-            #pragma omp parallel for shared(m_dataset, ids)
+//O programa se comportor melhor na leitura e escrita dos ids sem o uso de thread, mesmo com omp critical ainda estava dando erros de index nos arquivos
             for (int j = 0; j < m_dataset.size(); j++) {
-                    auto aux = ids.find(m_dataset[j][i]);
-                    if (aux != ids.end()) {
-                        m_dataset[j][i] = std::to_string(aux->second);
-                    }
-                    else {
-                        #pragma omp critical
-                        {
-                            ids[m_dataset[j][i]] = ids.size() + 1;
-                            m_dataset[j][i] = std::to_string(ids.size());
-                        }
-                    }
+                if (ids.find(m_dataset[j][i]) == ids.end()) {
+                    ids[m_dataset[j][i]] = ids.size();
+                }
             }
 
-            // Copy it into a vector.
-            std::vector<std::pair<std::string, int>> aux(ids.begin(), ids.end());
+#pragma omp parallel for
+            for (int j = 0; j < m_dataset.size(); j++) {
+                m_dataset[j][i] = std::to_string(ids.find(m_dataset[j][i])->second);
+            }
 
-            // Sort the vector according to the word count in descending order.
+            ids.erase(ids.begin());
+
+            std::vector<std::pair<std::string, int>> aux(ids.begin(), ids.end()); //passando para um vetor para a organizacao numerica
             std::sort(aux.begin(), aux.end(),
                 [](const auto& a, const auto& b)
                 { return a.second < b.second; });
 
-            dict.open(dictindex[i], std::ios::out | std::ios::trunc); //excluir conteudo se ja existir
+            std::ofstream dict_out(dictindex[i], std::ios::out | std::ios::trunc); // Excluir conteúdo se já existir
+            dict_out << "Id, Descrição\n";
             for (const auto& pair : aux) {
-                dict << std::to_string(pair.second) + "," + pair.first + "\n";
+                dict_out << std::to_string(pair.second) + "," + pair.first + "\n";
             }
-            dict.close();
-            ids.clear();
+            dict_out.close();
+            aux.clear();
         }
     }
 }
@@ -253,9 +130,10 @@ void readCircle() {
     int sum = 1;
     int aux = static_cast<int>((limit * 0.10));
     bool can_loop = true;
-    while (!dataset.eof() && can_loop|| limit > sum && can_loop) {
+    while (!dataset.eof() && can_loop || (limit + 1) >= sum && can_loop) {
+        m_dataset.clear();
         for (int i = 0; i < aux; i++) {
-                if (std::getline(dataset, Line) && limit > sum) {
+            if (std::getline(dataset, Line) && (limit + 1) >= sum) {
                     //separar linha unica em varios componentes para a matriz
                     std::istringstream StringLine(Line);
                     std::string aux;
@@ -264,36 +142,28 @@ void readCircle() {
                         V_aux.push_back(aux);
                     }
                     m_dataset.push_back(V_aux);
-                    Point++;
                 }
                 else {
-                    //std::cerr << "fim do arquivo." << std::endl;
                     can_loop = false;
                 }
                 sum++;
         }
-        teste();
-        WriteCsv();
-        m_dataset.clear();
-        //std::cout << "Got it" << std::endl;
+        datasetManipulation();
+        WriteCsv(sum);
     }
 }
 
 int main() {
     auto start_time = std::chrono::high_resolution_clock::now();
-    dataset.open("dataset_00_sem_virg_final.csv", std::ios::out | std::ios::trunc);
+    wdataset.open("dataset_00_sem_virg_final.csv", std::ios::out | std::ios::trunc);
     dataset.close();
     dataset.open("dataset_00_sem_virg.csv", std::ios::in | std::ios::out | std::ios::app); //arquivo
-    //dataset.open("testeMaior.csv", std::ios::in | std::ios::out | std::ios::app); //arquivo
-    dataset.imbue(std::locale(""));
 
     CreateDictArchive();
+    dataset << '\n';
     readCircle();
     dataset.close();
-    WriteCsv();
-    
-
-
+    wdataset.close();
 
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
