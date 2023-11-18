6c9eae64df831adb626caa2210a1351d00f2adbd
Matty <Matty@DESKTOP-M0K4I8C>
2023-11-18 15:30:05 -0300

    comecando cuda

---
+++ CUDA/CUDA/kernel.cu
@@ -0,0 +1,13 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+int main()
+{
+    FILE* dataset = fopen("dataset_00_sem_virg", "r");
+    FILE* wdataset = fopen("dataset_00_sem_virg_final", "w");
+
+    return 0;
+}
