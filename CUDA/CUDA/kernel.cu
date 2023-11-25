a1b952ac79fb88b47d11b738a10eebc99c6da538
Matty <Matty@DESKTOP-M0K4I8C>
2023-11-25 00:51:51 -0300

    dicionario

--- CUDA/CUDA/kernel.cu
+++ CUDA/CUDA/kernel.cu
@@ -1,71 +1,160 @@
-﻿
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include <string.h>
-#include <stdio.h>
-
-#define LIMIT 150//00000
-#define N (1000*1024)
-
-
-bool in_BlackList(char**, char[], int);
-
-void CreateDictFile(FILE* dataset, FILE* wdataset, char** BlackList, char dict_index[26][201], int sizeBlack) {
-    if (dataset != NULL) {
-        char Line[500];
-        fgets(Line, 500, dataset);
-        fprintf(wdataset, "%s\n" , Line); //escrever as colunas no arquivo final
-        char* StringLine = strtok(Line, ","); //dividir Linha em tokens, separados por ,
-        int i = 0;
-        FILE* dict;
-
-        while (StringLine != NULL) { //Enquanto houver tokens
-            char* actual = strdup(StringLine); //pegar token atual
-
-            //verificar se o valor esta na blacklist
-            if (in_BlackList(BlackList, actual, sizeBlack)) {
-                strcpy(dict_index[i], "b");
-            }
-            else {
-                snprintf(dict_index[i], 201, "dicts/%s.csv", actual);
-                dict = fopen(dict_index[i], "wb");
-                if (dict != NULL) {
-                    fprintf(dict, "id, descrição");
-                    //funcao para preencher id
-                    fclose(dict);
-                }
-            }
-            StringLine = strtok(NULL, ","); //Proximo token
-            i++;
-        }
-
-    }
-}
-
-bool in_BlackList(char** BackList, char Value[], int size) {
-
-    for (int i = 0; i < size; i++) {
-        if (strcmp(BackList[i], Value) == 0) {
-            return true;
-        }
-    }
-    return false;
-}
-
-void readCircle() {
-
-}
-
-int main()
-{
-    FILE* dataset = fopen("dataset_00_sem_virg.csv", "r");
-    FILE* wdataset = fopen("dataset_00_sem_virg_final.csv", "w");
-
-    char* Blacklist[] = { "idatracacao", "idcarga", "nacionalidadearmador", "pesocarga_cntr", "pesocargabruta", "qtcarga", "tatracado", "tesperaatracacao", "tesperacainicioop",
-"tesperadesatracacao", "testadia", "toperacao", "ano" }; //os seguintes nomes sao colunas de valores ja numericos, nao precisando de um tratamento de id para tais
-    char dict_index[26][201];
-
-    CreateDictFile(dataset,wdataset,Blacklist,dict_index, sizeof(Blacklist) / sizeof(Blacklist[0]));
-
-    return 0;
-}
+﻿//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+//#include <string.h>
+//#include <stdio.h>
+//#include <time.h>
+//
+//#define LIMIT 150//00000
+//#define N (1000*1024)
+//
+//
+//bool in_BlackList(char**, char[], int);
+//
+//void CreateDictFile(FILE* dataset, FILE* wdataset, char** BlackList, char dict_index[26][201], int sizeBlack) {
+//    if (dataset != NULL) {
+//        char Line[500];
+//        fgets(Line, 500, dataset);
+//        fprintf(wdataset, "%s\n" , Line); //escrever as colunas no arquivo final
+//        char* StringLine = strtok(Line, ","); //dividir Linha em tokens, separados por ,
+//        int i = 0;
+//        FILE* dict;
+//
+//        while (StringLine != NULL) { //Enquanto houver tokens
+//            char* actual = strdup(StringLine); //pegar token atual
+//
+//            //verificar se o valor esta na blacklist
+//            if (in_BlackList(BlackList, actual, sizeBlack)) {
+//                strcpy(dict_index[i], "b");
+//            }
+//            else {
+//                snprintf(dict_index[i], 201, "dicts/%s.csv", actual);
+//                dict = fopen(dict_index[i], "wb");
+//                if (dict != NULL) {
+//                    fprintf(dict, "id, descrição");
+//
+//                    //funcao para preencher id
+//                    fclose(dict);
+//                }
+//            }
+//            StringLine = strtok(NULL, ","); //Proximo token
+//            i++;
+//        }
+//
+//    }
+//}
+//
+//bool in_BlackList(char** BackList, char Value[], int size) {
+//
+//    for (int i = 0; i < size; i++) {
+//        if (strcmp(BackList[i], Value) == 0) {
+//            return true;
+//        }
+//    }
+//    return false;
+//}
+//
+//void datasetManipulation(char dict_index[26][201],int d_aux, char m_dataset[(int)(LIMIT * 0.10) + 1][26][201]) {
+//    char Line[500];
+//    char ids[LIMIT][201];
+//    
+//    for (int i = 0; i < 26; i++) {
+//        if(strlen(dict_index[i]) > 1) {
+//            int index = 0;
+//            FILE* dict = fopen(dict_index[i], "r");
+//            if (dict != NULL) {
+//                fgets(Line, 500, dict); //ler primeira linha para ignorar
+//                while (fgets(Line, 500, dict)) {
+//                    char* StringLine = strtok(Line, ",");
+//                    StringLine = strtok(NULL, ","); //ignorar primeira id
+//                    strcpy(ids[index], strdup(StringLine));
+//                    index++;
+//                }
+//                fclose(dict);
+//            }
+//
+//
+//            //preencher ids
+//            for (int j = d_aux+1; j > 0; j--) {
+//                bool in_id = false;
+//                for (int z = 0; z < index; z++) {
+//                    if (strcmp(ids[z], m_dataset[j][i]) == 0) {
+//                        in_id = true;
+//                    }
+//                }
+//                if (!in_id) {
+//                    strcpy(ids[index], m_dataset[j][i]);
+//                    printf("%s", ids[index]);
+//                    index++;
+//                }
+//            }
+//
+//            dict = fopen(dict_index[i], "w");
+//            if (dict != NULL) {
+//                fprintf(dict, "id, descrição\n");
+//                for (int j = 0; j < index; j++) {
+//                    fprintf(dict, "%d,%s\n", (j + 1), ids[j]);
+//                }
+//                fclose(dict);
+//            }
+//        }
+//    }
+//}
+//
+//void readCircle(FILE* dataset, char dict_index[26][201]) {
+//    int aux = (int)(LIMIT * 0.10) + 1;
+//    int sum = 1;
+//    char Line[500];
+//    bool can_loop = true;
+//    fgets(Line, sizeof(Line), dataset);
+//    
+//    while(LIMIT >= sum)
+//    {
+//        int d_aux = 0;
+//        char m_dataset[(int)(LIMIT * 0.10) + 1][26][201];
+//        while (fgets(Line, sizeof(Line), dataset) != NULL && aux > d_aux) {
+//            //printf("%s\n", Line);
+//            char* StringLine = strtok(Line, ",");
+//            int i = 0;
+//            while (StringLine != NULL) { //Enquanto houver tokens
+//                char* actual = strdup(StringLine); //pegar token atual
+//                //printf("%s\n", actual);
+//                if (strlen(actual) > 0) {
+//                    strcpy(m_dataset[d_aux][i], actual);
+//                }
+//                //printf(" %s ", m_dataset[d_aux][i]);
+//                StringLine = strtok(NULL, ",");
+//                i++;
+//            }
+//            //printf("%s ", m_dataset[][0]);
+//            //datasetManipulation(dict_index,d_aux, m_dataset);
+//            
+//            sum++;
+//            d_aux++;
+//        }
+//for (int z = 0; z < (int)(LIMIT * 0.10) + 1; z++) {
+//                printf("linha %d: ", z);
+//                for (int j = 0; j < 26; j++) {
+//                    //printf("%s ", m_dataset[z][j]);
+//                }
+//                printf("\n");
+//            }
+//
+//        
+//    }
+//    
+//}
+//
+//int main()
+//{
+//
+//    FILE* dataset = fopen("dataset_00_sem_virg.csv", "r");
+//    FILE* wdataset = fopen("dataset_00_sem_virg_final.csv", "w");
+//
+//    char* Blacklist[] = { "idatracacao", "idcarga", "nacionalidadearmador", "pesocarga_cntr", "pesocargabruta", "qtcarga", "tatracado", "tesperaatracacao", "tesperacainicioop",
+//"tesperadesatracacao", "testadia", "toperacao", "ano" }; //os seguintes nomes sao colunas de valores ja numericos, nao precisando de um tratamento de id para tais
+//    char dict_index[26][201];
+//
+//    CreateDictFile(dataset,wdataset,Blacklist,dict_index, sizeof(Blacklist) / sizeof(Blacklist[0]));
+//    readCircle(dataset, dict_index);
+//    return 0;
+//}
---
+++ CUDA/CUDA/ppc.cu
@@ -0,0 +1,171 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <chrono>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+#define limit 150//00000
+#define columns 26
+#define string_size 255
+
+std::fstream dataset;
+std::fstream wdataset;
+std::vector<std::string> dictindex;
+std::vector<std::vector<std::string>> m_dataset; //matriz que ira armazenar o dataset
+std::string Line; //string responsavel por armazenar a linha que atualmente esta sendo lida
+std::fstream dict;
+int number = 0;
+
+char h_ids[limit][12][string_size];
+
+std::vector<std::string> blacklist = { "idatracacao", "idcarga", "nacionalidadearmador", "pesocarga_cntr", "pesocargabruta", "qtcarga", "tatracado", "tesperaatracacao", "tesperacainicioop",
+"tesperadesatracacao", "testadia", "toperacao", "ano", "cdmercadoria_cntr"}; //os seguintes nomes sao colunas de valores ja numericos, nao precisando de um tratamento de id para tais
+
+bool in_blackList(std::string value) {
+    for (int i = 0; i < size(blacklist); i++) {
+        if (blacklist[i] == value) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//Funcao para ler a primeira linha do arquivo e criar os arquivos de dicionarios necessarios
+void CreateDictArchive() {
+    if (dataset.is_open()) {
+        if (std::getline(dataset, Line)) {
+            
+            wdataset << Line << std::endl; //escrever cabecalho no arquivo
+            std::istringstream StringLine(Line);
+            std::string data; //armazenar os valores separados por virguma
+            while (std::getline(StringLine, data, ',')) { //pegar o valor antes das/entre as virgula e armazenar em data
+                if (!in_blackList(data)) {
+                    dictindex.push_back("dicts/" + data + ".csv");
+                    number++;
+                    dict.open("dicts/" + data + ".csv", std::ios::out | std::ios::trunc); //excluir conteudo se ja existir
+                    dict.close();
+                }
+                else
+                {
+                    dictindex.push_back("b");
+                }
+            }
+        }
+    }
+}
+
+void WriteCsv(int sum) {
+
+    for (int i = 0; i < m_dataset.size(); i++) {
+        Line = m_dataset[i][0];
+        for (int j = 1; j < m_dataset[i].size(); j++) {
+            Line += "," + m_dataset[i][j];
+        }
+        wdataset << Line + '\n';
+    }
+
+}
+
+int Id(int indexc) {
+    std::map<std::string, int> ids;
+    int indexl = 0;
+    bool loop = true;
+    while (strlen(h_ids[indexl][indexc]) > 0) {
+        ids[h_ids[indexl][indexc]] = indexl + 1;
+        indexl++;
+    }
+
+
+    for (int j = 0; j < m_dataset.size(); j++) {
+        if (ids.find(m_dataset[j][indexc]) == ids.end()) {
+            ids[m_dataset[j][indexc]] = ids.size();
+            //strcpy(h_ids[indexl][indexc], "");
+            snprintf(h_ids[indexl][indexc], string_size, m_dataset[j][indexc].c_str(), indexl, indexc);
+            indexl++;
+        }
+    }
+
+    ids.clear();
+
+    return indexl;
+}
+
+
+void readCircle() {
+    int sum = 0;
+    int aux = static_cast<int>((limit * 0.10)); // dividir o servico em 10 partes
+    bool can_loop = true;
+
+    while (!dataset.eof() && can_loop || limit >= sum && can_loop) {
+        m_dataset.clear();
+        for (int i = 0; i < aux; i++) {
+            if (std::getline(dataset, Line) && limit >= sum) {
+                //separar linha unica em varios componentes para a matriz
+                std::istringstream StringLine(Line);
+                std::string z_aux;
+                std::vector<std::string> V_aux;
+                while (std::getline(StringLine, z_aux, ',')) {
+                    V_aux.push_back(z_aux);
+                }
+                m_dataset.push_back(V_aux);
+                //strcy(h_dataset[sum][i])
+
+            }
+            else {
+                can_loop = false;
+            }
+            sum++;
+        }
+        
+        int indexc = -1;
+        for (int i = 0; i < dictindex.size(); i++) {
+            if (dictindex[i].size() > 1) {
+                indexc++;
+                int length = Id(indexc);
+
+                std::ofstream dict_out(dictindex[i], std::ios::out | std::ios::trunc); // Excluir conteúdo se já existir
+                if(dict_out.is_open())
+                {
+                    dict_out << "Id, Descrição\n";
+                    for (int j = 0; j < length; j++) {
+                        //std::cout << h_ids[j][i] << " ";
+                        if(strlen(h_ids[j][i]) > 0) dict_out << std::to_string(j + 1) + "," + h_ids[j][i] + "\n";
+                    }
+                    dict_out.close();
+                }
+                //printf("%d ",length);
+            }
+        }
+
+        //datasetManipulation();
+        WriteCsv(sum);
+    }
+}
+
+
+int main() {
+    auto start_time = std::chrono::high_resolution_clock::now();
+    wdataset.open("dataset_00_sem_virg_final.csv", std::ios::out | std::ios::trunc);
+    //dataset.close();
+    dataset.open("dataset_00_sem_virg.csv", std::ios::in | std::ios::out | std::ios::app); //arquivo
+
+    CreateDictArchive();
+    //wdataset << '\n';
+    readCircle();
+    dataset.close();
+    wdataset.close();
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    std::cout << "Tempo de execução: " << duration << " milissegundos" << std::endl;
+
+    return 0;
+}
